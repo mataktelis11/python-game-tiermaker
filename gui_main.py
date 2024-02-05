6580f18ddc1c5fde38931834b090124adbcfb813
mataktelis11 <mataktelis01@gmail.com>
2024-02-05 04:37:06 +0200

    removed combobox

--- gui_main.py
+++ gui_main.py
@@ -18,7 +18,15 @@ app.minsize(600,600)
 
 
 def button_function():
-	game_data, image_path = game_parser2.search_game_title('Sonic Unleashed')
+
+
+	current_selection = listbox.get(listbox.curselection())
+
+	result = game_parser2.search_game_title(current_selection)
+
+	if result is None: return
+
+	game_data, image_path = result
 
 	img = (Image.open(image_path))
 	#Resize the Image using resize method
@@ -26,74 +34,67 @@ def button_function():
 
 	main_image.configure(light_image=resized_image,size=(500,650))
 
-def combobox_callback(choice):
-    print("combobox dropdown clicked:", choice)
-    
-def on_combobox_text_changed(index, value, op):
-	#print(f"on_combobox_text_changed {combobox.get()}")
-		
-	if len(combobox.get())==0:
-		return
-		
-	titles = game_parser2.search_game_inDB(combobox.get())
-	combobox.configure(values=titles)
-	#combobox.event_generate("<Button-1>") doesnt work
 
 def on_entry_text_changed(index, value, op):
-	#print(f"on_combobox_text_changed {combobox.get()}")
 		
 	if len(entry.get())==0:
+		listbox.delete(0, customtkinter.END)
 		return
 		
 	titles = game_parser2.search_game_inDB(entry.get())
 	listbox.delete(0, customtkinter.END)
-	for index, t in enumerate(titles):
-		listbox.insert(index,t)
+	for i, t in enumerate(titles):
+		listbox.insert(i,t)
 
 
-# Use CTkButton instead of tkinter Button
-# button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-# button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 
 
-# main_image = customtkinter.CTkImage(light_image=Image.open('Python.svg.png'),size=(300,300))
+	
 
-# my_label = customtkinter.CTkLabel(app, text="", image=main_image)
-# my_label.pack(pady=10)
+
 
 app.grid_rowconfigure((0, 1, 2, 3, 4, 5), weight=1)
 app.grid_columnconfigure(0, weight=1)
 
 
-v = customtkinter.StringVar()
-v.trace_add(["write"],on_combobox_text_changed)
-
-combobox = customtkinter.CTkComboBox(master=app,values=[],
-                                     variable=v,
-                                     command=combobox_callback)
-combobox.grid(row=0, column=0, padx=10, pady=10, sticky="wne")
 
 main_image = customtkinter.CTkImage(light_image=Image.open('Python.svg.png'),size=(300,300))
 main_image_label = customtkinter.CTkLabel(app, text="", image=main_image)
-main_image_label.grid(row=1, column=0, padx=10, pady=10)
+main_image_label.grid(row=2, column=0, padx=10, pady=10)
 
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-button.grid(row=2, column=0, padx=10, pady=10)
 
+
+
+searchFrame = customtkinter.CTkFrame(master=app)
+
+
+searchFrame.grid_rowconfigure(0, weight=1)
+searchFrame.grid_columnconfigure(0, weight=10)
+searchFrame.grid_columnconfigure(1, weight=1)
 
 entryString = customtkinter.StringVar()
 entryString.trace_add(["write"],on_entry_text_changed)
 
-entry = customtkinter.CTkEntry(master=app, 
+
+entry = customtkinter.CTkEntry(master=searchFrame, 
 							   placeholder_text="Type a game title here",
 							   textvariable=entryString)
-entry.grid(row=3, column=0, sticky="wes")
+entry.grid(row=0, column=0, sticky="wesn")
+
+button = customtkinter.CTkButton(master=searchFrame, 
+								 text="Search",
+								 command=button_function)
+button.grid(row=0, column=1, sticky="wesn")
+
+
+
+searchFrame.grid(row=0, column=0, sticky="wes")
 
 listbox = CTkListbox(master=app)
 
 
 
 
-listbox.grid(row=4, column=0, sticky="wne")
+listbox.grid(row=1, column=0, sticky="wnes")
 
 app.mainloop()
