73852b2d31137ae396683a04156399086c6f9df0
mataktelis11 <mataktelis01@gmail.com>
2024-02-08 22:52:42 +0200

    removable labels in container frame

--- gui_search.py
+++ gui_search.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import customtkinter
+import tkinter 
 from PIL import Image
 
 from GameBrowserFrame import GameBrowserFrame
@@ -36,27 +37,9 @@ class App(customtkinter.CTk):
         self.containerFrame.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
 
         self.contained_guids = []
-
-
-    def append_image(self, frame, image_path):
-        # Load the image
-        pillow_image = Image.open(image_path)
-
-
-        image_ratio = pillow_image.size[0] / pillow_image.size[1]
-        container_ratio = 1.0
-
-        if container_ratio > image_ratio:
-            height = 100
-            width = int(height * image_ratio)
-        else:
-            width = 100
-            height = int(width / image_ratio)
-
-        main_image = customtkinter.CTkImage(light_image=pillow_image, size=(width,height))
-        label = customtkinter.CTkLabel(frame, image=main_image, text='')        
-        label.pack(side= customtkinter.RIGHT, padx=5, pady=5)
+        self.contained_labels = []
         
+
 
     def append_game_to_container(self, guid):
         
@@ -84,6 +67,32 @@ class App(customtkinter.CTk):
         label.pack(side= customtkinter.RIGHT, padx=5, pady=5)
 
 
+        # source:   https://stackoverflow.com/questions/3296893/how-to-pass-an-argument-to-event-handler-in-tkinter
+        #           https://www.youtube.com/watch?v=VnwDPa9biwc        
+        data = {"guid": guid}
+        label.bind("<Button-1>", lambda event, arg=data: self.clicked_image(event, arg)) 
+
+        self.contained_labels.append(label)
+
+
+
+    def clicked_image(self, event, arg):
+        print(f'image was clicked with guid:{arg["guid"]}')
+        self.remove_game_from_container(arg["guid"])
+
+    
+    def remove_game_from_container(self, guid):
+        
+        if guid not in self.contained_guids:
+            print('Game is not in container')
+            return
+        
+        index = self.contained_guids.index(guid)
+        self.contained_guids.pop(index)
+
+        self.contained_labels[index].destroy()
+        self.contained_labels.pop(index)
+
 
     def append_image_button(self):
         guids = ['3030-20464', '3030-25305', '3030-45214', '3030-64883', '3030-20953', '3030-30196',
@@ -93,6 +102,7 @@ class App(customtkinter.CTk):
             
             self.append_game_to_container(g)
 
+        print(self.contained_guids)
 
 
 app = App()
