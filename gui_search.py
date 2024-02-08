17441feba8c1f67b3597f595676f917e3e47b9aa
mataktelis11 <mataktelis01@gmail.com>
2024-02-08 22:23:47 +0200

    working on container logic

--- gui_search.py
+++ gui_search.py
@@ -4,44 +4,95 @@ import customtkinter
 from PIL import Image
 
 from GameBrowserFrame import GameBrowserFrame
+import game_parser2
 
 
         
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        self.geometry("1000x800")
+        self.geometry("800x1200")
         self.minsize(600,600)
         self.grid_rowconfigure(0, weight=10) 
         self.grid_rowconfigure(1, weight=1) 
         self.grid_columnconfigure((0), weight=1)
 
-        self.gamebrowserframe = GameBrowserFrame(master=self)
 
+        self.tabview = customtkinter.CTkTabview(master=self)
+        self.tabview.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+
+        self.tabview.add("Tier list")  # add tab at the end
+        self.tabview.add("Game Search")  # add tab at the end
+        self.tabview.set("Game Search")  # set currently visible tab
+
+        self.gamebrowserframe = GameBrowserFrame(master=self.tabview.tab("Game Search"))
+        self.gamebrowserframe.pack(fill=customtkinter.BOTH, expand=1)
 
         self.gamebrowserframe.append_button.configure(command=self.append_image_button)
 
-        self.gamebrowserframe.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
+        #self.gamebrowserframe.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 
-        self.containerFrame = customtkinter.CTkFrame(master=self)
-        self.containerFrame.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
+        self.containerFrame = customtkinter.CTkScrollableFrame(master=self, orientation='horizontal', height=120)
+        self.containerFrame.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
 
-        for i in range(5):
-            self.append_image(self.containerFrame,'book.jpg', row=0, column=i)
+        self.contained_guids = []
 
 
-    def append_image(self, frame, image_path, row, column):
+    def append_image(self, frame, image_path):
         # Load the image
-        img = Image.open(image_path)
+        pillow_image = Image.open(image_path)
 
-        main_image = customtkinter.CTkImage(light_image=img, size=(100,100))
 
-        label = customtkinter.CTkLabel(frame, image=main_image, text='')
+        image_ratio = pillow_image.size[0] / pillow_image.size[1]
+        container_ratio = 1.0
+
+        if container_ratio > image_ratio:
+            height = 100
+            width = int(height * image_ratio)
+        else:
+            width = 100
+            height = int(width / image_ratio)
+
+        main_image = customtkinter.CTkImage(light_image=pillow_image, size=(width,height))
+        label = customtkinter.CTkLabel(frame, image=main_image, text='')        
+        label.pack(side= customtkinter.RIGHT, padx=5, pady=5)
         
-        label.grid(row=row, column=column, padx=2, pady=2)
+
+    def append_game_to_container(self, guid):
+        
+        if guid in self.contained_guids:
+            print('Game already in container')
+            return
+
+        self.contained_guids.append(guid)
+        game_data, image_path = game_parser2.search_cache_data(guid)
+
+        # Load the image
+        pillow_image = Image.open(image_path)
+        image_ratio = pillow_image.size[0] / pillow_image.size[1]
+        container_ratio = 1.0
+
+        if container_ratio > image_ratio:
+            height = 100
+            width = int(height * image_ratio)
+        else:
+            width = 100
+            height = int(width / image_ratio)
+
+        main_image = customtkinter.CTkImage(light_image=pillow_image, size=(width,height))
+        label = customtkinter.CTkLabel(self.containerFrame, image=main_image, text='')        
+        label.pack(side= customtkinter.RIGHT, padx=5, pady=5)
+
+
 
     def append_image_button(self):
-        print("hello from gui_search.py!")
+        guids = ['3030-20464', '3030-25305', '3030-45214', '3030-64883', '3030-20953', '3030-30196',
+                 '3030-73838', '3030-21170', '3030-31463', '3030-59912']
+        
+        for g in guids:
+            
+            self.append_game_to_container(g)
+
 
 
 app = App()
